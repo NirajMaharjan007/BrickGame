d0f5bd996c0f9f33f20fcf34b80877401bd28acc
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-10 12:45:02 +0545

    First

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,5 +1,8 @@
+import operation.MainOperation;
+
 public class MainActivity {
     public static void main(String[] args) throws Exception {
         System.out.println("MainActivity.main()");
+        new MainOperation();
     }
 }
---
+++ src/operation/MainOperation.java
@@ -0,0 +1,12 @@
+package operation;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MainOperation extends JFrame {
+    public MainOperation() {
+        setVisible(true);
+        setBackground(Color.WHITE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
