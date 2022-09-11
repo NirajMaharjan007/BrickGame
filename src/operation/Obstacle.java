88fd07e8586d3614fdb6b593248e34e1946dbdb9
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-11 22:27:09 +0545

    Added code in Obstacle

--- src/operation/Obstacle.java
+++ src/operation/Obstacle.java
@@ -11,6 +11,11 @@ public class Obstacle {
     public Obstacle() {
     }
 
+    public String getScreenSize() {
+        String str = "screen_width: " + screen_width + " and screen_height: " + screen_height;
+        return str;
+    }
+
     public Obstacle(int width, int height) {
         this.screen_height = height;
         this.screen_width = width;
@@ -18,20 +23,19 @@ public class Obstacle {
 
     public void drawObstacle(Graphics g) {
         g.setColor(Color.RED);
-        g.fillRect(x, y, 80, 25);
+        g.fillRect(x, y, 80, 20);
     }
 
     public void setObstacle() {
         Random random = new Random();
         x = random.nextInt(screen_width / 35) * 35;
-        y = random.nextInt(0, 5) * 35;
+        y = random.nextInt(3, 8) * 35;
     }
 
     public void isTouched() {
         if (new Rectangle(OperationPanel.ballX, OperationPanel.ballY, 25, 25)
-                .intersects(new Rectangle(x, y, 80, 25))) {
+                .intersects(new Rectangle(x, y, 80, 20))) {
             OperationPanel.ballYDir = -OperationPanel.ballYDir;
         }
     }
-
 }
